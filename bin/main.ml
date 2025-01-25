0d792ddfcf147edb46b90e74884dd3624b42bab5
frectonz <fraol0912@gmail.com>
2025-01-25 18:45:11 +0300

    fix: remove `stdint`

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,14 @@
-open Stdint
-
 let memory_max = 1 lsl 16
-let pc_start = Uint16.of_int 0x3000 (* Program counter starting address. *)
+let pc_start = 0x3000 (* Program counter starting address. *)
 let ( let* ) x f = match x with Ok v -> f v | Error e -> Error e
 
 module ConditionFlags = struct
   type t = FL_POS | FL_ZRO | FL_NEG
 
   let to_int = function
-    | FL_POS -> 1 lsl 0 |> Uint16.of_int
-    | FL_ZRO -> 1 lsl 1 |> Uint16.of_int
-    | FL_NEG -> 1 lsl 2 |> Uint16.of_int
+    | FL_POS -> 1 lsl 0
+    | FL_ZRO -> 1 lsl 1
+    | FL_NEG -> 1 lsl 2
 
   let fl_pos = to_int FL_POS
   let fl_zro = to_int FL_ZRO
@@ -46,28 +44,28 @@ end
 
 module Registers = struct
   type t = {
-    r_r0 : uint16;
-    r_r1 : uint16;
-    r_r2 : uint16;
-    r_r3 : uint16;
-    r_r4 : uint16;
-    r_r5 : uint16;
-    r_r6 : uint16;
-    r_r7 : uint16;
-    r_pc : uint16; (* Program Counter *)
-    r_cond : uint16; (* Condition Register *)
+    r_r0 : int;
+    r_r1 : int;
+    r_r2 : int;
+    r_r3 : int;
+    r_r4 : int;
+    r_r5 : int;
+    r_r6 : int;
+    r_r7 : int;
+    r_pc : int; (* Program Counter *)
+    r_cond : int; (* Condition Register *)
   }
 
   let make () =
     {
-      r_r0 = Uint16.zero;
-      r_r1 = Uint16.zero;
-      r_r2 = Uint16.zero;
-      r_r3 = Uint16.zero;
-      r_r4 = Uint16.zero;
-      r_r5 = Uint16.zero;
-      r_r6 = Uint16.zero;
-      r_r7 = Uint16.zero;
+      r_r0 = 0;
+      r_r1 = 0;
+      r_r2 = 0;
+      r_r3 = 0;
+      r_r4 = 0;
+      r_r5 = 0;
+      r_r6 = 0;
+      r_r7 = 0;
       (* Set program counter to the starting point. *)
       r_pc = pc_start;
       (* Condition flag shoul alway be set. *)
@@ -77,14 +75,14 @@ module Registers = struct
   let r_pc registers = registers.r_pc
 
   let inc_r_pc registers =
-    { registers with r_pc = Uint16.(registers.r_pc + one) }
+    { registers with r_pc = registers.r_pc + 1 }
 end
 
 module Memory = struct
-  type t = uint16 array
+  type t = int array
 
-  let empty () = Array.make memory_max Uint16.zero
-  let get memory pos = Array.get memory (Uint16.to_int pos)
+  let empty () = Array.make memory_max 0
+  let get memory pos = Array.get memory pos
 
   let make image_path =
     let memory = empty () in
@@ -99,13 +97,13 @@ module Memory = struct
             let read = In_channel.input ic buf 0 max_read in
             for pos = 0 to read / 2 do
               let n = Bytes.get_uint16_be buf (pos * 2) in
-              memory.(origin + pos) <- n |> Uint16.of_int
+              memory.(origin + pos) <- n
             done;
             Ok memory)
 end
 
 module OpCode = struct
-  type register_or_value = Value of uint16 | Register of Register.t
+  type register_or_value = Value of int | Register of Register.t
 
   type t =
     | OP_BR (* branch *)
@@ -127,31 +125,20 @@ module OpCode = struct
     | OP_TRAP (* execute trap *)
 
   let sign_extend x bit_count =
-    let bit_count = bit_count - 1 in
-    let open Uint16 in
-    if logand (shift_right_logical x bit_count) (of_int 1) = of_int 1 then
-      logor x (shift_right_logical (of_int 0xFFFF) bit_count)
-    else x
+    if ((x lsr (bit_count - 1)) land 1) = 1 then
+      x lor (0xFFFF lsl bit_count)
+    else
+      x
 
   let parse_add instr =
-    let imm_flag =
-      Uint16.(logand (shift_right_logical instr 5) (Uint16.of_int 1))
-    in
-    let* dr =
-      Uint16.(logand (shift_right_logical instr 9) (of_int 0x7))
-      |> Uint16.to_int |> Register.of_int
-    in
-    let* sr1 =
-      Uint16.(logand (shift_right_logical instr 6) (of_int 0x7))
-      |> Uint16.to_int |> Register.of_int
-    in
-    if imm_flag == Uint16.of_int 1 then
-      let v = sign_extend Uint16.(logand instr (of_int 0x1F)) 5 in
+    let imm_flag = (instr lsr 5) land 1 in
+    let* dr = (instr lsr 9) land 0x7 |> Register.of_int in
+    let* sr1 = (instr lsr 6) land 0x7 |> Register.of_int in
+    if imm_flag == 1 then
+      let v = sign_extend (instr land 0x1F) 5 in
       Ok (OP_ADD { dr; sr1; sr2 = Value v })
     else
-      let* r =
-        Uint16.(logand instr (of_int 0x7)) |> Uint16.to_int |> Register.of_int
-      in
+      let* r = instr land 0x7 |> Register.of_int in
       Ok (OP_ADD { dr; sr1; sr2 = Register r })
 end
 
@@ -167,8 +154,8 @@ module Program = struct
   let rec evolve program =
     let registers = Registers.inc_r_pc program.registers in
     let instr = Registers.r_pc registers |> Memory.get program.memory in
-    let op = Uint16.(shift_right_logical instr 12) in
-    match op |> Uint16.to_int with
+    let op = instr lsr 12 in
+    match op  with
     | 1 -> OpCode.parse_add instr
     | x -> Error (`UnknownOp x)
 end
