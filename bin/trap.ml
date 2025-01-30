aea482be23aa0b90dc7a9e291d936eb1a5e47469
frectonz <fraol0912@gmail.com>
2025-01-30 09:29:57 +0300

    feat: impl rendering trap

--- bin/trap.ml
+++ bin/trap.ml
@@ -16,6 +16,15 @@ module Trap = struct
     | TRAP_HALT -> 0x25
   ;;
 
+  let to_string = function
+    | TRAP_GETC -> "TRAP_GETC"
+    | TRAP_OUT -> "TRAP_OUT"
+    | TRAP_PUTS -> "TRAP_PUTS"
+    | TRAP_IN -> "TRAP_IN"
+    | TRAP_PUTSP -> "TRAP_PUTSP"
+    | TRAP_HALT -> "TRAP_HALT"
+  ;;
+
   let of_int = function
     | 0x20 -> Ok TRAP_GETC
     | 0x21 -> Ok TRAP_OUT
