c22b8a0980a8391ad14ec331a69ebf7a796e570b
frectonz <fraol0912@gmail.com>
2025-01-30 09:29:09 +0300

    feat: impl rendering registers

--- bin/registers.ml
+++ bin/registers.ml
@@ -76,4 +76,30 @@ module Registers = struct
   let set_r_r5 v = set ~index:R_R5 ~value:v
   let set_r_r6 v = set ~index:R_R6 ~value:v
   let set_r_r7 v = set ~index:R_R7 ~value:v
+
+  let render (registers : t) =
+    let open Nottui in
+    let module W = Nottui_widgets in
+    let module A = Notty.A in
+    let reg_line name value =
+      W.hbox
+        [ W.string ~attr:A.(fg green ++ st bold) (Printf.sprintf "%-4s" name)
+          |> Lwd.return
+        ; W.string ~attr:A.(fg (gray 5)) "0x" |> Lwd.return
+        ; W.string ~attr:A.(fg white) (Printf.sprintf "%04x" value) |> Lwd.return
+        ]
+    in
+    W.vbox
+      [ Utils.header "REGISTERS"
+      ; Ui.space 0 2 |> Lwd.return
+      ; reg_line "R0" registers.r_r0
+      ; reg_line "R1" registers.r_r1
+      ; reg_line "R2" registers.r_r2
+      ; reg_line "R3" registers.r_r3
+      ; reg_line "R4" registers.r_r4
+      ; reg_line "R5" registers.r_r5
+      ; reg_line "R6" registers.r_r6
+      ; reg_line "R7" registers.r_r7
+      ]
+  ;;
 end
